cc4d298fd7d5adbe57806ed9597b004d9917d426
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2019-09-14 13:58:20 -0500

    kitchen orders

--- src/app/pages/kitchen-page/kitchen-page.component.ts
+++ src/app/pages/kitchen-page/kitchen-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { WsService } from '../../services/ws.service';
+import { WebSocketSubject } from 'rxjs/internal-compatibility';
+import { Order } from '../../models/order';
 
 @Component({
   selector: 'app-kitchen-page',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kitchen-page.component.scss']
 })
 export class KitchenPageComponent implements OnInit {
+  kitchenSocket: WebSocketSubject<any>;
+  orders: Order[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private ws: WsService) {
   }
 
+  ngOnInit() {
+    this.kitchenSocket = this.ws.getKitchenSocket();
+    this.kitchenSocket.subscribe(
+      (data) => this.eventConsumer(data),
+      (error) => console.log(error),
+      () => console.warn('closed socket')
+    );
+  }
+
+  private eventConsumer(data) {
+    if (data.type === 'initial_orders') {
+      this.orders = data.orders;
+    }
+  }
+
+  ordersType(type: number) {
+    return this.orders.filter((order) => order.state === type);
+  }
 }
